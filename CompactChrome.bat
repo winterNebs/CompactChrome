5937b84663cb15db4e73fdfa2924aee118e0f3e6
Isaac <isaac@wen.ca>
2020-02-15 15:33:26 -0500

    Fixed window not working

--- CompactChrome.bat
+++ CompactChrome.bat
@@ -2,5 +2,5 @@
 Title Fullscreen Chrome
 SET /P weburl= Enter URL (Example: "google.com" or "twitch.tv"):
 ECHO  USE CHROME DIRECTORY
-SET weburl= "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"  --profile-directory=Default --app=http://%weburl%
+SET weburl= "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"  --new-window --app=http://%weburl%
 %weburl%
